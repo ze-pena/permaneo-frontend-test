2b68646e1ace69a4e5a6f39fd12fe6903c29646e
ze-pena <jose.pena.dev@gmail.com>
2025-04-27 22:45:05 -0300

    feat: create "user" file to store all the fake course api request

---
+++ src/app/data/user.ts
@@ -0,0 +1,18 @@
+export const user = {
+  id: 1,
+  name: 'John Doe',
+  courses: [
+    {
+      courseId: 1,
+      dateJoined: '2023-02-15',
+    },
+    {
+      courseId: 3,
+      dateJoined: '2023-03-10',
+    },
+    {
+      courseId: 5,
+      dateJoined: '2023-04-05',
+    },
+  ],
+};
