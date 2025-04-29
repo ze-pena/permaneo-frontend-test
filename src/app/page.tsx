a44f4e0967deb0663c1eef2d88c34dc957717f3b
ze-pena <jose.pena.dev@gmail.com>
2025-04-29 14:38:40 -0300

    refactor: build home using ressources already created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,40 @@
 'use client';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from './store/hooks';
+import { getCourses } from './store/reducers/courses';
+
+import CourseCard from './components/cards/CourseCard';
+import SpinnerLoading from './components/loaders/SpinnerLoading';
+
+import styles from './page.module.scss';
 
 export default function Home() {
+  const dispatch = useAppDispatch();
+  const { courses, isLoading } = useAppSelector(state => state.courses);
+
+  useEffect(() => {
+    dispatch(getCourses());
+  }, [dispatch]);
+
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <div className={styles['home']}>
+      <h1 className={styles['home__title']}>
+        Aprenda conosco e avance na sua carreira com trilhas de aprendizado feitas sob medida.
+      </h1>
+
+      <div className={styles['home__body']}>
+        {isLoading ? (
+          <SpinnerLoading />
+        ) : (
+          <ul className={styles['home__body__course-list']}>
+            {courses.map(course => (
+              <li key={course.id}>
+                <CourseCard {...course} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
