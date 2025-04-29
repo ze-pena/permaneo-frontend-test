d6c24567190f29d99ab24ea614b1164cd0c40172
ze-pena <jose.pena.dev@gmail.com>
2025-04-29 14:37:46 -0300

    refactor: update store adding new properties and actions

--- src/app/store/middlewares/courses.ts
+++ src/app/store/middlewares/courses.ts
@@ -1,17 +1,77 @@
-import { getCourses, initCoursesState } from '@/app/store/reducers/courses';
+import {
+  setLoading,
+  getCourses,
+  initCoursesState,
+  getSelectedCourse,
+  setSelectedCourse,
+} from '@/app/store/reducers/courses';
+
 import { createListenerMiddleware } from '@reduxjs/toolkit';
+
+import { RootState } from '../store';
+
 import services from '@/app/services';
 
 export const coursesListener = createListenerMiddleware();
 
 coursesListener.startListening({
   actionCreator: getCourses,
-  effect: async (_, { fork, dispatch }) => {
+  effect: async (_, { fork, dispatch, getState, unsubscribe }) => {
+    dispatch(setLoading(true));
+
+    const {
+      user: { user },
+    } = getState() as RootState;
+
     const task = fork(async () => await services.courses.getCourses());
     const response = await task.result;
 
     if (response.status === 'ok') {
-      dispatch(initCoursesState(response.value));
+      const state = response.value.map(course => {
+        const is_purchased = user.courses.some(userCourse => userCourse.courseId === course.id);
+        const is_favorite = user.favorites.some(
+          userFavorite => userFavorite.courseId === course.id
+        );
+        const video_progress = 0;
+        return { ...course, is_purchased, is_favorite, video_progress };
+      });
+
+      dispatch(initCoursesState(state));
+      unsubscribe();
     }
+  },
+});
+
+coursesListener.startListening({
+  actionCreator: getSelectedCourse,
+  effect: async (action, { fork, dispatch, getState }) => {
+    dispatch(setLoading(true));
+
+    const paramCourseId = action.payload;
+    const {
+      user: { user },
+      courses: { courses },
+    } = getState() as RootState;
+
+    if (!courses.length) {
+      const task = fork(async () => await services.courses.getCourses());
+      const response = await task.result;
+
+      if (response.status === 'ok') {
+        const state = response.value.map(course => {
+          const is_purchased = user.courses.some(userCourse => userCourse.courseId === course.id);
+          const is_favorite = user.favorites.some(
+            userFavorite => userFavorite.courseId === course.id
+          );
+          const video_progress = 0;
+          return { ...course, is_purchased, is_favorite, video_progress };
+        });
+
+        dispatch(initCoursesState(state));
+      }
+    }
+
+    const courseSelectedState = courses.find(course => course.id === paramCourseId) || null;
+    dispatch(setSelectedCourse(courseSelectedState));
   },
 });
--- src/app/store/provider.tsx
+++ src/app/store/provider.tsx
@@ -3,11 +3,19 @@ import { useRef } from 'react';
 import { Provider } from 'react-redux';
 import { makeStore, AppStore } from './store';
 
-export default function StoreProvider({ children }: React.PropsWithChildren) {
+import { initUserState } from './reducers/user';
+import { User } from '../interfaces/User';
+
+interface Props extends React.PropsWithChildren {
+  user: User;
+}
+
+export default function StoreProvider({ user, children }: Props) {
   const storeRef = useRef<AppStore | null>(null);
 
   if (!storeRef.current) {
     storeRef.current = makeStore();
+    storeRef.current.dispatch(initUserState(user));
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>;
