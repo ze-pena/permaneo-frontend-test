ff0182d10478fd14c09646d46dd960af19322334
ze-pena <jose.pena.dev@gmail.com>
2025-04-29 14:32:49 -0300

    refactor: decrease "fakeRequest" request time

--- src/app/config/fakeRequest.ts
+++ src/app/config/fakeRequest.ts
@@ -1,6 +1,6 @@
 export default async function fakeRequest<T>(data: T): Promise<T> {
   const promise = new Promise<T>(resolve => {
-    setTimeout(() => resolve(data), 2000);
+    setTimeout(() => resolve(data), 1000);
   });
   const result = await promise.then(value => value);
   return result;
